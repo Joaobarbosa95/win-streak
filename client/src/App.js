710071997c99c59c92d0f151540f49a317e84d56
Joaobarbosa95 <bticjoaobarbosa@hotmail.com>
2022-04-13 15:32:39 +0100

    routing added

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,17 @@ import "./App.css";
 import Login from "./Components/Login/Login";
 import CreateAccount from "./Components/Login/CreateAccount";
 
+// Router
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <CreateAccount />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="create-account" element={<CreateAccount />} />
+      </Routes>
     </div>
   );
 }
